04915903e5308479943a0453ca59107ecece0503
Evandro Carreira <evandro.carreira@gmail.com>
2016-09-18 20:49:48 -0300

    teste de autopull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Tesseract</div>
+                <div class="title">Tesseract autopull</div>
             </div>
         </div>
     </body>
