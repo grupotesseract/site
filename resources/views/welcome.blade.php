3ce025b826d7a446640cd83c155b55c229361cc4
Evandro Carreira <evandro.carreira@gmail.com>
2016-09-18 21:12:07 -0300

    pull hit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Tesseract autopull</div>
+                <div class="title">Tesseract pull</div>
             </div>
         </div>
     </body>
