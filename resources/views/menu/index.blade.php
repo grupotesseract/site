3dd7feb64cc0f416cfb4fff71368c2f3f96998c7
Emanuel Larini <emanuellarini@hotmail.com>
2016-10-13 06:50:25 -0300

    Fix some names in menu for a better readability

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,7 +1,7 @@
 <aside id="menu" class="well sidebar-nav">
     <h4><i class="fa fa-home"></i> <b>Geral</b></h4>
     <ul class="nav nav-pills nav-stacked">
-        <li><a href="{{ url('team-tesseract') }}">Listar</a></li>
+        <li><a href="{{ url('team-tesseract') }}">Listar Integrantes</a></li>
     </ul>
 
     <hr>
@@ -10,6 +10,6 @@
     <ul class="nav nav-pills nav-stacked">
         <li><a href="{{ url('team-tesseract/skills') }}">Conhecimentos</a></li>
         <li><a href="{{ url('team-tesseract/roles') }}">Funções</a></li>
-        <li><a href="{{ url('team-tesseract/create') }}">Time</a></li>
+        <li><a href="{{ url('team-tesseract/create') }}">Integrantes</a></li>
     </ul>
 </aside>
