a44d9424c191d00e59851cf7c9eddb55ef8efb91
Emanuel Larini <emanuellarini@hotmail.com>
2016-10-13 06:51:35 -0300

    Add center h1 in view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
         <div class="container">
 
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-12 text-center">
                     <legend>
                         <h1>Grupo Tesseract</h1>
                     </legend>
