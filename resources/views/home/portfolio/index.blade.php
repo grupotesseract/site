e49f1368d29082eee0e2f0364f508ff122ab336e
Evandro Carreira <evandro.carreira@gmail.com>
2016-12-05 16:57:59 -0200

    links vivala

--- resources/views/home/portfolio/index.blade.php
+++ resources/views/home/portfolio/index.blade.php
@@ -2,26 +2,35 @@
 <section id="portfolio" class="container">
   <div class="parallax text-center primeiro-item padding-4">
       <div class="info-projeto">
-        <h2 class="titulo-trabalho">Song of the sea</h2>
-        <h3 class="itens-trabalho text-muted">
-          banco de dados
-          <br />
-          ecommerce
-          <br />
-          branding
-        </h3>
+        <h2 class="titulo-trabalho">Vivalá</h2>
+        <div class="links row">
+          <a class="itens-trabalho text-muted col-xs-12" href="https://docs.vivala.com.br">
+            documentação
+          </a>
+          <a class="itens-trabalho text-muted col-xs-12" href="https://github.com/somosvivala/vivala">
+            github
+          </a>
+          <a class="itens-trabalho text-muted col-xs-12" href="https://vivala.com.br">
+            site
+          </a>
+        </div>
       </div>
   </div>
   <div class="parallax text-center segundo-item padding-4">
       <div class="info-projeto">
         <h2 class="titulo-trabalho">Sen to Chihiro</h2>
-        <h3 class="itens-trabalho text-muted">
-          banco de dados
-          <br />
-          ecommerce
-          <br />
-          branding
-        </h3>
+
+        <div class="links row">
+          <a class="itens-trabalho text-muted col-xs-12" href="https://docs.vivala.com.br">
+            documentação
+          </a>
+          <a class="itens-trabalho text-muted col-xs-12" href="https://github.com/somosvivala/vivala">
+            github
+          </a>
+          <a class="itens-trabalho text-muted col-xs-12" href="https://vivala.com.br">
+            site
+          </a>
+        </div>
       </div>
   </div>
 </section>
