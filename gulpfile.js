1b67d4e8cfcbb7257e60359d0b8609e984c1c650
Bruno Luiz <brunoluizgr@gmail.com>
2016-12-06 15:23:05 -0200

    Alterações na estrutura do gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -24,29 +24,15 @@ elixir(function(mix) {
         '/../../../node_modules/sweetalert2/dist/sweetalert2.min.css',
     ], 'public/css/app.css');
 
-    mix.scripts([
-      /*
-        Já tão sendo incluídos via require/browserify
+    mix.browserify('app.js');
 
-        '/../../../node_modules/jquery/dist/jquery.min.js',
-        '/../../../node_modules/tether/dist/js/tether.min.js',
-        '/../../../node_modules/tether-tooltip/dist/js/tooltip.min.js',
-        '/../../../node_modules/tether-drop/dist/js/drop.min.js',
-        '/../../../node_modules/bootstrap/dist/js/bootstrap.min.js',
-        '/../../../node_modules/scrollreveal/dist/scrollreveal.min.js',
-        '/../../../node_modules/smooth-scroll/dist/js/smooth-scroll.min.js',
-        '/../../../node_modules/masonry-layout/dist/masonry.pkgd.min.js',
-        '/../../../node_modules/pace/pace.js',
-        '/../../../node_modules/sweetalert2/dist/sweetalert2.min.js',
-      */
+    mix.scripts([
         'vendor/wordrotate.js'
     ], 'public/js/tesseract.js');
 
     mix.sass([
     	'tesseract.scss'
     ], 'public/css/tesseract.css');
-
-    mix.browserify('app.js');
 
     mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
 });
