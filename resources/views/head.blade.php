bcad5938db39f916c2c25f998930a0d63d352c7a
Renato Gomes <renatoo.agomes@gmail.com>
2016-12-20 08:39:02 -0200

    adicionado cod de trackeamento do google analytics

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -83,7 +83,7 @@
           <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
       <![endif]-->
 
-    {{-- Google Analytics --}
+    {{-- Google Analytics --}}
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
